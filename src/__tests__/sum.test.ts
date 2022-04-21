113a1014677f782a07e65a212b7f0c9e0a871ca0
Alexandre Lim <alexandre.lim94@gmail.com>
2022-04-21 21:17:55 +0200

    chore(template): delete template example files

--- src/__tests__/sum.test.ts
+++
@@ -1,8 +0,0 @@
-import { sum } from '../sum';
-
-describe('sum', () => {
-  it('should return 5', () => {
-    const result = sum(3, 2);
-    expect(result).toBe(5);
-  });
-});
--- src/sum.spec.ts
+++
@@ -1,8 +0,0 @@
-import { sum } from './sum';
-
-describe('sum', () => {
-  it('should return 8', () => {
-    const result = sum(4, 4);
-    expect(result).toBe(8);
-  });
-});
